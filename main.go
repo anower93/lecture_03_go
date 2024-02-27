99a8a6f5462109b33ce9f40dd7b5021c8cf71124
Anower_Hoshin <anowerhossin1993@gmail.com>
2024-02-28 03:39:34 +0600

    Array Map

--- main.go
+++ main.go
@@ -2,12 +2,31 @@
 
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func main() {
 
-	// We are learning array Map
+	// We are learning array Map, It is called Associative array in PHP
 
 	// An Array is a composite data type or data structure
 
-	
+	x := make(map[string]string)
+
+	x["name"] = "Anower"
+	x["age"] = "30"
+	x["height"] = "5.5"
+
+	//We can delete any item from map
+
+	delete(x, "age")
+
+	fmt.Println(x)
+
+	data := reflect.TypeOf(x).Kind().String()
+
+	fmt.Println(data)
 
 }
