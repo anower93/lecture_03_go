859d9db4ab79b83bba88ae07ac0b1d90da225091
Anower_Hoshin <anowerhossin1993@gmail.com>
2024-02-28 02:17:53 +0600

    initial commit

--- main.go
+++ main.go
@@ -1,33 +1,11 @@
-//Now we are learning array
+//Now we are learning array Slice
 
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	// We are learning array
+	// We are learning array Slice
 
-	var students [3]string
-
-	students[0] = "Tahmid"
-	students[1] = "Aksa"
-	students[2] = "Abu Ubaida"
-
-	fmt.Println(students) // In this way we can declare array and pull data, retrieve data or get data
-
-	// Or we can write in short hand
-
-	teachers := [3]string{"Tahmid", "Abu Ubaida", "Aksa"}
-
-	fmt.Println(teachers)
-
-	//We can import unlimited data implicitly
-
-	ssc := [...]string{"Aman", "Zaman", "Rahman", "Lalcan"}
-
-	fmt.Println(ssc)
+	// An Array is a composite data type or data structure
 
 }
