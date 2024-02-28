e43679441525e5af5b8eed05a47c2e52742a9df7
Anower_Hoshin <anowerhossin1993@gmail.com>
2024-02-28 10:27:20 +0600

    practise struct

--- main.go
+++ main.go
@@ -9,6 +9,25 @@ import (
 
 func main() {
 
-	
+
+type Book struct {
+	name string
+	price float32
+	pages int
+}
+
+
+var b1 Book 
+
+b1.name = "Go in Bangla"
+b1.price = 13.13
+b1.pages = 100
+
+fmt.Println(b1)
+fmt.Println(b1.name)
+fmt.Println(b1.pages)
+
+
+
 
 }
